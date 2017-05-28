116689fdcbe35ad47be7a8e145342593953f508e
Peter Crest <p.crest@outlook.com>
2017-05-29 00:06:58 +0100

    Added the Project cards

--- logick.js
+++ logick.js
@@ -1,6 +1,9 @@
 document.getElementById("Projects").hidden = true;
 document.getElementById("About").hidden = true;
 
+document.getElementById("TasksDes").textContent = MyTasks;
+document.getElementById("AppiumDes").textContent = MyAppium;
+
 function ShowAbout() {
     document.getElementById("Hello").hidden = true;
     document.getElementById("Projects").hidden = true;
